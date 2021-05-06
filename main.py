8acfe774911bf58c2c2545c6cf7642ffd2b44a89
bkuscu <bkuscu@techfak.de>
2021-05-06 15:14:58 +0200

    the bot token is safe now, stored the token in a .env file.

--- main.py
+++ main.py
@@ -1,12 +1,17 @@
-import random
+import os
 import discord
+import random
+from dotenv import load_dotenv
+
+load_dotenv()
+
 
 class MyClient(discord.Client):
-    #Login method
+    # Login method
     async def on_ready(self):
         print("Server started..")
 
-    #Triggers when a message is send (all kinds of messages)
+    # Triggers when a message is send (all kinds of messages)
     async def on_message(self, message):
         if message.author == client.user:
             return
@@ -14,5 +19,7 @@ class MyClient(discord.Client):
         if message.content.startswith("$msg"):
             await message.channel.send("Command call by: " + str(message.author))
             await message.author.send("You used the command $msg")
+
+
 client = MyClient()
-client.run("ODM5NjA0NDA3NDkwNzA3NDc2.YJMEiQ.arS0qfaKPEw3CyXeCzEtM56mJMw")
+client.run(os.getenv("DISCORD_TOKEN"))
